a0ac9978c3aa473bd47ed8a9a719b42e2ad1a6a6
Sergei Kolobov <sergei@kolobov.com>
2021-02-02 20:15:14 +0300

    feat: require JIRA references in commit messages (DBAAS-4703)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,12 @@
 // commitlint.config.js
 module.exports = {
-  extends: ['@commitlint/config-conventional'], // => commitlint/config-conventional
+    extends: ['@commitlint/config-conventional'], // => commitlint/config-conventional
+    'rules': {
+      'references-empty': [2, 'never'],
+    },
+    parserPreset: {
+      parserOpts: {
+        issuePrefixes: ['DBAAS-']
+      }
+    },
 };
